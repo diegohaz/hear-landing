8acd111f6e0c4b1fb7fa30a54a0245b84a676262
Diego Haz <hazdiego@gmail.com>
2016-05-22 17:35:19 -0300

    Update english

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,7 +4,7 @@ import styles from './Banner.scss'
 const Banner = () => (
   <div className={styles.banner}>
     <p>Leave your favorite <strong>music</strong> in the place you are</p>
-    <p>Listen music from other people while walking in the <strong>city</strong></p>
+    <p>Listen to music from other people while walking in the <strong>city</strong></p>
   </div>
 )
 
