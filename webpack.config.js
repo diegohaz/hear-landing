e1c9cbb7b535af5eb36bd33f8f41c46c1053c231
Diego Haz <hazdiego@gmail.com>
2016-10-09 03:52:01 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
-var path = require('path');
-var webpack = require('webpack');
-var autoprefixer = require('autoprefixer');
+var path = require('path')
+var autoprefixer = require('autoprefixer')
 
 module.exports = {
   entry: [
@@ -16,17 +15,17 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/,    loader: 'babel', exclude: /node_modules/ },
-      { test: /\.s?css$/, loader: 'style!css?camelCase&modules&importLoaders=2&localIdentName=[name]__[local]___[hash:base64:5]!sass!postcss' },
-      { test: /\.png$/,   loader: 'url?prefix=images/&limit=8000&mimetype=image/png' },
-      { test: /\.jpg$/,   loader: 'url?prefix=images/&limit=8000&mimetype=image/jpeg' },
-      { test: /\.svg$/,   loader: 'url?prefix=images/&limit=8000&mimetype=image/svg+xml' },
-      { test: /\.woff$/,  loader: 'url?prefix=fonts/&limit=8000&mimetype=application/font-woff' },
-      { test: /\.ttf$/,   loader: 'file?prefix=fonts/' },
-      { test: /\.eot$/,   loader: 'file?prefix=fonts/' }
+      {test: /\.js$/, loader: 'babel', exclude: /node_modules/},
+      {test: /\.s?css$/, loader: 'style!css?camelCase&modules&importLoaders=2&localIdentName=[name]__[local]___[hash:base64:5]!sass!postcss'},
+      {test: /\.png$/, loader: 'url?prefix=images/&limit=8000&mimetype=image/png'},
+      {test: /\.jpg$/, loader: 'url?prefix=images/&limit=8000&mimetype=image/jpeg'},
+      {test: /\.svg$/, loader: 'url?prefix=images/&limit=8000&mimetype=image/svg+xml'},
+      {test: /\.woff$/, loader: 'url?prefix=fonts/&limit=8000&mimetype=application/font-woff'},
+      {test: /\.ttf$/, loader: 'file?prefix=fonts/'},
+      {test: /\.eot$/, loader: 'file?prefix=fonts/'}
     ]
   },
-  postcss: function() {
-    return [autoprefixer];
+  postcss: function () {
+    return [autoprefixer]
   }
-};
+}
